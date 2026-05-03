bc613587cd6b78bcf6f57909848e7e37006b798a
Annie <annieyu0425@gmail.com>
2026-05-04 01:24:36 +0800

    show all posts

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
     query = sort === 'top'
       ? query.order('same_count', { ascending: false })
       : query.order('created_at', { ascending: false })
-    const { data } = await query.limit(50)
+    const { data } = await query.limit(1000)
     if (data) { setPosts(data as Post[]); setTotalToday(data.length) }
   }
 
